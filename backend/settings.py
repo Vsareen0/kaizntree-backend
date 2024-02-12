a3710b4e952069447b844780fb23174ed5310495
Vinamra Sareen <vsareen24@gmail.com>
2024-02-13 03:52:32 +0530

    repositioned middlware

--- backend/settings.py
+++ backend/settings.py
@@ -54,10 +54,10 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'inventory.auth_middleware.JWTAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'inventory.auth_middleware.JWTAuthenticationMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
