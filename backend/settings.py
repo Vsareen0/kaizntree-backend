25d88a6746e7882f0c94d1a9bb76699145576a8f
Vinamra Sareen <vsareen24@gmail.com>
2024-02-13 10:02:54 +0530

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nocodenexus@gmail.com'
-EMAIL_HOST_PASSWORD = 'oazvqofqcjdwvtlq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 LOGGING = {
     'version': 1,
