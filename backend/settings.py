b0c7f1b75f0dab09f070e15599d37d5b4a9033ec
Vinamra Sareen <vsareen24@gmail.com>
2024-02-13 09:41:38 +0530

    removed secrets

--- backend/settings.py
+++ backend/settings.py
@@ -86,17 +86,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kaizntree',
-        'USER' : 'postgres',
-        'PASSWORD' : 'postgres',
-        'HOST' : 'kaizntree-1.cd64eac4644h.ap-south-1.rds.amazonaws.com',
-        'PORT' : '5432',
+        'NAME': '', # username
+        'USER' : '', # user
+        'PASSWORD' : '', # password
+        'HOST' : '', # host
+        'PORT' : '', #port
     }
 }
 
